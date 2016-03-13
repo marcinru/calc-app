790c5de980dda09ca574ec0f0d6c6f8b7c5113bf
Marcin Rudzki <mar.rudzki@gmail.com>
2016-03-13 11:24:02 +0100

    Variable names refactoring

--- calc.ctrl.js
+++ calc.ctrl.js
@@ -1,18 +1,18 @@
-angular.module('calcApp', []).controller('calcCtrl', function($scope) {
-	var cart = this;
-	cart.shipping_options = [
-		{ name: 'Flat Rate', cost: 10 },
-		{ name: 'Express', cost: 20 },
-		{ name: 'Super Express', cost: 30 }
-	];
-	cart.price = 100;
-	cart.incl_tax = true;
-	cart.shipping = cart.shipping_options[0];
-	$scope.$watch('cart.price', updateView);
-	$scope.$watch('cart.incl_tax', updateView);
-	$scope.$watch('cart.shipping', updateView);
-	function updateView() {
-		cart.tax = cart.incl_tax ? cart.price * 0.25 : 0;
-		cart.total = parseFloat(cart.price) + cart.tax + cart.shipping.cost;
-	}
+angular.module('calc', []).controller('cartCtrl', function($scope) {
+    var cart = this;
+    cart.shippingOptions = [
+        { name: 'Flat Rate', cost: 10 },
+        { name: 'Express', cost: 20 },
+        { name: 'Super Express', cost: 30 }
+    ];
+    cart.price = 0;
+    cart.inclTax = true;
+    cart.shipping = cart.shippingOptions[0];
+    $scope.$watch('cart.price', updateView);
+    $scope.$watch('cart.inclTax', updateView);
+    $scope.$watch('cart.shipping', updateView);
+    function updateView() {
+        cart.tax = cart.inclTax ? cart.price * 0.25 : 0;
+        cart.total = +cart.price + cart.tax + cart.shipping.cost;
+    }
 });
